3dea6fd8cc67f2c7f297b78f49ccd4159a39f416
Cameron Wright <cameronwright@mediaworks.co.nz>
2016-04-01 11:16:20 +1300

    Cleared out console calls, corrected fonts and colours, added radio button styles
    Removed console calls in the JS.
    Changed font call to go through CSS.
    Styled buttons with pseudo states.
    Added rounded radio button styles.
    Commented out rare cases for input fields to make it easier to see what
    bit of code I’m actually working on.

--- assets/components/js/custom.js
+++ assets/components/js/custom.js
@@ -1,16 +1,13 @@
 // detects when a field is empty
 jQuery.fn.extend({
 	checkEmpty: function() {
-		console.log('check empty');
 		var $this = $(this);
 		if ($this.val() === '') {
-			console.log('empty');
 			$this
 				.addClass('empty')
 				.removeClass('notempty')
 			;
 		} else {
-			console.log('not empty');
 			$this
 				.addClass('notempty')
 				.removeClass('empty')
@@ -18,74 +15,49 @@ jQuery.fn.extend({
 		}
 	},
 	floatLabel: function() {
-		console.log('float label');
 		var $this = $(this);
 		$this.checkEmpty();
 		$this.bind("change paste keyup", function() {
-			console.log('event');
 			$this.checkEmpty();
 		});
 	}
 });
 
+// inits the isempty check
 $('input[type=text],input[type=password],input[type=email],input[type=number],input[type=tel],input[type=search],input[type=url],input[type=message],select,textarea').each(function() {
 	$(this).floatLabel();
-	console.log('init');
 });
 
-// detects when a checkbox or radio button has been selected
-
-
-
-// http://thecodeplayer.com/walkthrough/ripple-click-effect-google-material-design
-var parent, ink, d, x, y;
-$("button").click(function(e){
-	parent = $(this).parent();
-	//create .ink element if it doesn't exist
-	if(parent.find(".ink").length == 0)
-		parent.prepend("<span class='ink'></span>");
-		
-	ink = parent.find(".ink");
-	//incase of quick double clicks stop the previous animation
-	ink.removeClass("animate");
-	
-	//set size of .ink
-	if(!ink.height() && !ink.width())
-	{
-		//use parent's width or height whichever is larger for the diameter to make a circle which can cover the entire element.
-		d = Math.max(parent.outerWidth(), parent.outerHeight());
-		ink.css({height: d, width: d});
-	}
-	
-	//get click coordinates
-	//logic = click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center;
-	x = e.pageX - parent.offset().left - ink.width()/2;
-	y = e.pageY - parent.offset().top - ink.height()/2;
-	
-	//set the position and add class .animate
-	ink.css({top: y+'px', left: x+'px'}).addClass("animate");
-})
+// handles the click feedback animation for buttons
+$('button').each(function(){
+	var $this = $(this);
+	$this.on('click',function(){
+		$this.removeClass('clicked');
+		$this.addClass('clicked');
+	});
+});
 
 
 // http://www.impressivewebs.com/textarea-auto-resize/
 $(function() {
-    var txt = $('#textarea'),
-        hiddenDiv = $(document.createElement('div')),
-        content = null;
+	var txt = $('#textarea'),
+		hiddenDiv = $(document.createElement('div')),
+		content = null
+	;
 
-    txt.addClass('txtstuff');
-    hiddenDiv.addClass('hiddendiv common');
+	txt.addClass('txtstuff');
+	hiddenDiv.addClass('hiddendiv common');
 
-    $('body').append(hiddenDiv);
+	$('body').append(hiddenDiv);
 
-    txt.on('keyup', function () {
+	txt.on('keyup', function () {
 
-        content = $(this).val();
+		content = $(this).val();
 
-        content = content.replace(/\n/g, '<br>');
-        hiddenDiv.html(content + '<br class="lbr">');
+		content = content.replace(/\n/g, '<br>');
+		hiddenDiv.html(content + '<br class="lbr">');
 
-        $(this).css('height', hiddenDiv.outerHeight());
+		$(this).css('height', hiddenDiv.outerHeight());
 
-    });​
+	});​
 });
